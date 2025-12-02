f58ea2111aab7a414bbdc5fabfc848bfb2dfee62
Kat <168232325+klinares23@users.noreply.github.com>
2025-12-01 23:45:29 -0600

    Add files via upload
    leetcode medium - diameter of binary tree
    https://leetcode.com/problems/diameter-of-binary-tree/description/

---
+++ diameterOfBinaryTree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+    
+private:
+    int height(TreeNode* node, int& diameter) {
+        if (!node) return 0;
+        
+        int leftHeight = height(node->left, diameter);
+        int rightHeight = height(node->right, diameter);
+        
+        diameter = max(diameter, leftHeight + rightHeight);
+        
+        return 1 + max(leftHeight, rightHeight);
+    }
+};
