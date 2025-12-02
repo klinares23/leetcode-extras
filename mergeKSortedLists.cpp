b5fadce0d43282f3e0707c5d464b98e7f00bcfef
Kat <168232325+klinares23@users.noreply.github.com>
2025-12-01 23:40:33 -0600

    Add files via upload
    leetcode hard - merge k sorted lists
    https://leetcode.com/problems/merge-k-sorted-lists/description/

---
+++ mergeKSortedLists.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+        return mergeRange(lists, 0, lists.size() - 1);
+    }
+    
+private:
+    ListNode* mergeRange(vector<ListNode*>& lists, int left, int right) {
+        if (left == right) return lists[left];
+        if (left > right) return nullptr;
+        
+        int mid = left + (right - left) / 2;
+        ListNode* l1 = mergeRange(lists, left, mid);
+        ListNode* l2 = mergeRange(lists, mid + 1, right);
+        
+        return mergeTwoLists(l1, l2);
+    }
+    
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        
+        curr->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
